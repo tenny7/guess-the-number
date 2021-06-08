631bc94f0da71e930639a0f6d65fa1021a16983c
Ovie Tennyson <atomictenny@gmail.com>
2021-06-08 08:19:25 +0000

    First Version of the guessing game app

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,1 +1,23 @@
-print('Guess the Number')
+import random
+
+
+class GuessTheNumber:
+    # generate random number from 1 - 100, instantiate tries to 1,
+    # continue loop until user input matches the generated number
+    generated_number = random.randint(1, 100)
+    user_input = int(input("Guess the number: "))
+    tries = 1
+    while user_input != generated_number:
+        if user_input < generated_number:
+            print("Go higher")
+            user_input = int(input("Guess the number: "))
+            tries += 1
+            break
+        elif user_input == generated_number:
+            print("Hurray, you guessed right")
+        else:
+            print("Go lower")
+            user_input = int(input("Guess the number: "))
+            tries += 1
+    if tries == 7:
+        input("You have exhausted your tries. Subscribe for more. \n Press any key to exit!")
