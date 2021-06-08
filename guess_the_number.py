577143b4b9eab0f2a85f50794adf2edae830b377
Qay-Tekkie <gkay260@gmail.com>
2021-06-08 12:35:53 +0000

    scratch code

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,1 +1,20 @@
-print('Guess the Number')
+import random, math
+
+secret_number = random.randint(1, 10)
+
+guess_number = None
+
+count = 0
+while count < 3:
+    count += 1
+
+    guess_number = int(input("Guess the number:  "))
+
+    if guess_number == secret_number:
+        print('You won')
+        break
+    else:
+        print('try again')
+
+if count >= 3:
+    print("Game over")
